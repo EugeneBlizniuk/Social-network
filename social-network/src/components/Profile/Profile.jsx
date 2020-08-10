bbb5b8ae7541aa9b33cf4229e4941c567ad07ee8
Eugen Bliznyuk <eugenbliznyuk@coherentsolutions.com>
2020-08-10 23:21:42 +0300

    adding new functionality, working on user finder

--- social-network/src/components/Profile/Profile.jsx
+++ social-network/src/components/Profile/Profile.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import style from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import Users from "./Users/Users";
 
 const Profile = () => {
   return (
     <div className={style.profile}>
       <ProfileInfo />
       <MyPostsContainer />
+      <Users />
     </div >
   );
 };
---
+++ social-network/src/components/Profile/Users/Users.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            Users are going to be here
+        </div>
+    );
+}
+
+export default Users;
