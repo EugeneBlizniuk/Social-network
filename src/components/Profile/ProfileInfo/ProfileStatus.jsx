eadeaf3e6db8b4fa1a58e1618bf2bf640da8fb37
Eugene Blizniuk <janey1102@mail.ru>
2020-08-22 16:47:56 +0300

    refactored code a bit, added autoFocus to the profile status field

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -5,15 +5,13 @@ class ProfileStatus extends React.Component {
         editMode: false
     };
 
-    activateEditMode() {
-        debugger
+    activateEditMode = () => {
         this.setState({
             editMode: true
         });
     }
 
-    deactivateEditMode() {
-        debugger
+    deactivateEditMode = () => {
         this.setState({
             editMode: false
         });
@@ -24,12 +22,12 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
-                        <input onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}></input>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}></input>
                     </div>
                 }
             </div>
