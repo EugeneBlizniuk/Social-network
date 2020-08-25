fd87699f78b1db075d6d3a0ea98b3ad19c6f34e5
Eugene Blizniuk <janey1102@mail.ru>
2020-08-25 08:10:03 +0300

    found a bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = (props) => {
 
       <div className={style.loginBlock}>
         {props.isAuthenticated
-          ? <div> props.login <button onClick={props.logout}>Log out</button></div> 
+          ? <div> { props.login } <button onClick={props.logout}>Log out</button></div> 
           : <NavLink to={'/login'}>Login</NavLink>}
       </div>
     </header>
