00feeb565c7352e3d2c4c95ec583495a4c318b47
EugeneBlizniuk <janey1102@mail.ru>
2020-08-28 07:12:56 +0300

    displaying a default avatar right now

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import style from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
 import ProfileStatus from './ProfileStatus';
+import userPhoto from '../../../assets/images/user.png';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -12,7 +13,7 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div className={style.descriptionBlock}>
-                <img src={props.profile.photos.large} />
+                <img src={props.profile.photos.large || userPhoto} className={style.avatar} />
                 <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
             </div>
         </div>
